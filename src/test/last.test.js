c2b2d1c1103e2be1b2cff61dba26856e819e7a48
xiongdawang <xiongdawang@yeah.net>
2019-07-10 17:27:32 +0800

    Create last.test.js

---
+++ src/test/last.test.js
@@ -0,0 +1,27 @@
+import { last } from "../last";
+import { of } from "../of";
+import { toArray } from "../toArray";
+
+test("last", async () => {
+    const result = await toArray(last()(of(1, 2, 3, 4, 5, 6, 7)));
+    expect(result).toEqual([7]);
+});
+
+test("last with predicate", async () => {
+    const result = await toArray(last((value) => value % 2 === 0)(of(1, 2, 3, 4, 5, 6, 7)));
+    expect(result).toEqual([6]);
+});
+
+test("last with no value fulfilling predicate", async () => {
+    const result = await toArray(
+        last((value) => value % 8 === 0)(of(1, 2, 3, 4, 5, 6))
+    );
+    expect(result).toEqual([]);
+});
+
+test("last with default valaue", async () => {
+    const result = await toArray(
+        last((value) => value % 8 === 0, 42)(of(1, 2, 3, 4, 5, 6))
+    );
+    expect(result).toEqual([42]);
+});
