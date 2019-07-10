8da49425744809ee9e794eb6a6da332126bfe70e
xiongdawang <xiongdawang@yeah.net>
2019-07-10 17:19:45 +0800

    Update pipe.js

--- src/pipe.js
+++ src/pipe.js
@@ -2,7 +2,7 @@ export function pipe(...funs) {
     return function inner(source) {
         let current = source;
         for (fun of funs) {
-            current = fun(source);
+            current = fun(current);
         }
         return current;
     }
