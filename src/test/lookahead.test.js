66343b6ec0288e7f844c596199bbfffd9313d554
xiongdawang <xiongdawang@yeah.net>
2019-07-10 17:27:49 +0800

    Create lookahead.test.js

---
+++ src/test/lookahead.test.js
@@ -0,0 +1,9 @@
+import { lookahead } from '../lookahead';
+import { of } from '../of';
+import { toArray } from '../toArray';
+
+test("lookahead", async () => {
+    const result = await lookahead(2)(of(1, 2, 3));
+    expect(result.values).toEqual([1, 2]);
+    expect(await toArray(result.iterable)).toEqual([1, 2, 3]);
+});
