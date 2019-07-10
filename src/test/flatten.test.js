9da320a4947b24e10aa32221b738ecf5927e19b3
xiongdawang <xiongdawang@yeah.net>
2019-07-10 17:27:16 +0800

    Create flatten.test.js

---
+++ src/test/flatten.test.js
@@ -0,0 +1,8 @@
+import { flatten } from '../flatten';
+import { of } from '../of';
+import { toArray } from '../toArray';
+
+test("flatten", async () => {
+    const result = await toArray(flatten(of(of(1), of(2), of(3))));
+    expect(result).toEqual([1, 2, 3]);
+});
