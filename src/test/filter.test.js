25ab67e63fae849188f1992ec5f3ebc34a247025
xiongdawang <xiongdawang@yeah.net>
2019-07-10 17:26:24 +0800

    Create filter.test.js

---
+++ src/test/filter.test.js
@@ -0,0 +1,10 @@
+import { filter } from '../filter';
+import { of } from '../of';
+import { toArray } from '../toArray';
+
+test("filter", async () => {
+    const result = await toArray(
+        filter((value) => value % 2 === 0)(of(1, 2, 3, 4, 5, 6))
+    );
+    expect(result).toEqual([2, 4, 6]);
+});
