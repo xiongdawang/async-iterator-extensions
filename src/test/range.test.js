fce8a7dd90318261ae2dbe8c620eb4cb9c2cd693
xiongdawang <xiongdawang@yeah.net>
2019-07-10 17:28:44 +0800

    Create range.test.js

---
+++ src/test/range.test.js
@@ -0,0 +1,7 @@
+import { range } from "../range";
+import { toArray } from "../toArray";
+
+test("range", async () => {
+    const result = await toArray(range(1, 3));
+    expect(result).toEqual([1, 2]);
+});
