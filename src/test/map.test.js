4a4748969c0c8b76b5b7405e7ec5d28aa7c5b238
xiongdawang <xiongdawang@yeah.net>
2019-07-10 17:28:06 +0800

    Create map.test.js

---
+++ src/test/map.test.js
@@ -0,0 +1,10 @@
+import { map } from "../map";
+import { of } from "../of";
+import { toArray } from "../toArray";
+
+test("map", async () => {
+  const result = await toArray(
+    map((value) => value * 2)(of(1, 2, 3))
+  );
+  expect(result).toEqual([2, 4, 6]);
+});
